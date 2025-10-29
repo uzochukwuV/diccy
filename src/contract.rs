bd0cdb3f59400334eb5f9e8e01a8388966018ca6
uzochukwuv <vic.ezealor@gmail.com.com>
2025-10-29 13:54:41 +0100

    first commit

---
+++ src/contract.rs
@@ -0,0 +1,102 @@
+#![cfg_attr(target_arch = "wasm32", no_main)]
+
+mod state;
+
+use linera_sdk::{
+    linera_base_types::WithContractAbi,
+    views::{RootView, View},
+    Contract, ContractRuntime,
+};
+
+use esport::Operation;
+
+use self::state::EsportState;
+
+pub struct EsportContract {
+    state: EsportState,
+    runtime: ContractRuntime<Self>,
+}
+
+linera_sdk::contract!(EsportContract);
+
+impl WithContractAbi for EsportContract {
+    type Abi = esport::EsportAbi;
+}
+
+impl Contract for EsportContract {
+    type Message = ();
+    type Parameters = ();
+    type InstantiationArgument = u64;
+    type EventValue = ();
+
+    async fn load(runtime: ContractRuntime<Self>) -> Self {
+        let state = EsportState::load(runtime.root_view_storage_context())
+            .await
+            .expect("Failed to load state");
+        EsportContract { state, runtime }
+    }
+
+    async fn instantiate(&mut self, argument: Self::InstantiationArgument) {
+        // validate that the application parameters were configured correctly.
+        self.runtime.application_parameters();
+        self.state.value.set(argument);
+    }
+
+    async fn execute_operation(&mut self, operation: Self::Operation) -> Self::Response {
+        match operation {
+            Operation::Increment { value } => {
+                self.state.value.set(self.state.value.get() + value);
+            }
+        }
+    }
+
+    async fn execute_message(&mut self, _message: Self::Message) {}
+
+    async fn store(mut self) {
+        self.state.save().await.expect("Failed to save state");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use futures::FutureExt as _;
+    use linera_sdk::{util::BlockingWait, views::View, Contract, ContractRuntime};
+
+    use esport::Operation;
+
+    use super::{EsportContract, EsportState};
+
+    #[test]
+    fn operation() {
+        let initial_value = 10u64;
+        let mut app = create_and_instantiate_app(initial_value);
+
+        let increment = 10u64;
+
+        let _response = app
+            .execute_operation(Operation::Increment { value: increment })
+            .now_or_never()
+            .expect("Execution of application operation should not await anything");
+
+        assert_eq!(*app.state.value.get(), initial_value + increment);
+    }
+
+    fn create_and_instantiate_app(initial_value: u64) -> EsportContract {
+        let runtime = ContractRuntime::new().with_application_parameters(());
+        let mut contract = EsportContract {
+            state: EsportState::load(runtime.root_view_storage_context())
+                .blocking_wait()
+                .expect("Failed to read from mock key value store"),
+            runtime,
+        };
+
+        contract
+            .instantiate(initial_value)
+            .now_or_never()
+            .expect("Initialization of application state should not await anything");
+
+        assert_eq!(*contract.state.value.get(), initial_value);
+
+        contract
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use async_graphql::{Request, Response};
+use linera_sdk::{
+    graphql::GraphQLMutationRoot,
+    linera_base_types::{ContractAbi, ServiceAbi},
+};
+use serde::{Deserialize, Serialize};
+
+pub struct EsportAbi;
+
+impl ContractAbi for EsportAbi {
+    type Operation = Operation;
+    type Response = ();
+}
+
+impl ServiceAbi for EsportAbi {
+    type Query = Request;
+    type QueryResponse = Response;
+}
+
+#[derive(Debug, Deserialize, Serialize, GraphQLMutationRoot)]
+pub enum Operation {
+    Increment { value: u64 },
+}
